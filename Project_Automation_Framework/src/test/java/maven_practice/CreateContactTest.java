0fc019af23f819752ed087cafb0ae55ee7c13606
RohanGit011 <rohan.tewary7857@gmail.com>
2025-05-09 17:02:02 +0530

    First Push

---
+++ Project_Automation_Framework/src/test/java/maven_practice/CreateContactTest.java
@@ -0,0 +1,19 @@
+package maven_practice;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	
+	@Test
+	public void demoTest()
+	{
+		System.out.println("This is demo method");
+	}
+	
+	@Test
+	public void finalTest()
+	{
+		System.out.println("This is final method");
+	}
+
+}
---
+++ Project_Automation_Framework/src/test/java/maven_practice/CreateOrganisationTest.java
@@ -0,0 +1,19 @@
+package maven_practice;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganisationTest {
+	
+	@Test
+	public void mockTest()
+	{
+		System.out.println("This is mock test method");
+	}
+	
+	@Test
+	public void trial()
+	{
+		System.out.println("This is trial method");
+	}
+
+}
